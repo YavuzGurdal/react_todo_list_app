f752ed0042b4800a5b465c5851b12abc518a46e5
Yavuzgrdl <32365733+Yavuzgrdl@users.noreply.github.com>
2020-03-30 23:27:28 +0200

    Updates

---
+++ precache-manifest.fd984da433152e8cf19a69e7bca2a04c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b4032148490b39ccd2dd9d6b6415bf",
+    "url": "/react_todo_list_app/index.html"
+  },
+  {
+    "revision": "206cf287696ed0a0fa53",
+    "url": "/react_todo_list_app/static/css/2.48fe9ecd.chunk.css"
+  },
+  {
+    "revision": "b3ead50f98f8a99e9a0d",
+    "url": "/react_todo_list_app/static/css/main.d6ea186b.chunk.css"
+  },
+  {
+    "revision": "206cf287696ed0a0fa53",
+    "url": "/react_todo_list_app/static/js/2.16c96184.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "/react_todo_list_app/static/js/2.16c96184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ead50f98f8a99e9a0d",
+    "url": "/react_todo_list_app/static/js/main.aa904014.chunk.js"
+  },
+  {
+    "revision": "d3d03238a59b37a05627",
+    "url": "/react_todo_list_app/static/js/runtime-main.fecc7045.js"
+  }
+]);
